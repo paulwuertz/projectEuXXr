038f4662448f80d2f849b0346bdaed2b1fcaa473
paulwuertz <paulwuertz@posteo.de>
2017-11-10 17:46:48 +0100

    Added more Readmes and fixed other minor README issues

--- genSolvedHTML.py
+++ genSolvedHTML.py
@@ -1,14 +1,15 @@
 import os
 
 readme="""
-* [Problem 1-50](#Problem-1)
-* [Problem 50-100](#Problem-50)
-* [Problem 101-150](#Problem-100)
-* [Problem 151-200](#Problem-150)
+* [Problem 1-50](#problem-1)
+* [Problem 50-100](#problem-50)
+* [Problem 101-150](#problem-100)
+* [Problem 151-200](#problem-150)
+
 ---"""
 
 for file in sorted(os.listdir("solved")):
-    readme+="\n\n###Problem-"+file.replace(".c","")+"\n\n"
+    readme+="\n\n### Problem-"+file.replace(".c","")+"\n\n"
     readme+=open("solved/"+file).read().split("*/")[0].replace("/*","")+"\n\n---"
 
 f=open("solved/README.md","w")
