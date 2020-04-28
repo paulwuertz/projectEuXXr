33326394ecb64bb1e8aac3cfb5dc804f5fbb39a5
Paul Würtz <paulwuertz@posteo.de>
2020-04-28 16:31:23 +0200

    change problem 1 for hackerrank euler+

--- c/solved/001.c
+++ c/solved/001.c
@@ -5,19 +5,34 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 ---
 
+### project euler
+
 Just a loop and a condition, boring...
+
+### hacker rank
+
+The loop thing was to slow for two test.
+I changed it to use the euler formular for sums.
+
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]){
-    int a = atoi(argv[1]);
-    int sum=0;
-    //process from one to 'a'
-    for (int i = 0; i < a; ++i){
-        //add if divisible by 3 or 5 to sum
-        if(i%3==0 || i%5==0) sum+=i;
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){    
+        int a;
+        scanf("%d",&a);
+        a--;
+        unsigned long long s3 = (a/3);
+        unsigned long long s5 = (a/5);
+        unsigned long long s15 = (a/15);
+        
+        unsigned long long sum3 =  3* ((s3*s3+s3)/2);
+        unsigned long long sum5 =  5* ((s5*s5+s5)/2);
+        unsigned long long sum15 = 15*((s15*s15+s15)/2);
+        printf("%lu\n", sum3+sum5-sum15);
     }
-    printf("%i\n", sum);
     return 0;
 }
